d60c29f67759544bb1ce1f5edf4d3e8672ca40fe
hu-le-xiang <hulx@mail.ustc.edu.cn>
2021-12-12 11:54:42 +0800

    update

--- Student/task2/cpp/assign_gen.cpp
+++ Student/task2/cpp/assign_gen.cpp
@@ -1,3 +1,64 @@
+#include "BasicBlock.h"
+#include "Constant.h"
+#include "Function.h"
+#include "IRStmtBuilder.h"
+#include "Module.h"
+#include "Type.h"
+
+#include <iostream>
+#include <memory>
+
+#ifdef DEBUG
+#define DEBUG_OUTPUT std::cout << __LINE__ << std::endl;
+#else
+#define DEBUG_OUTPUT
+#endif
+
+#define CONST_INT(num) \
+    ConstantInt::get(num, module)
+
+#define CONST_FP(num) \
+    ConstantFloat::get(num, module)
+
 int main(){
+    auto module = new Module("SysY code");
+    auto builder = new IRStmtBuilder(nullptr, module);
+    Type *Int32Type = Type::get_int32_type(module);
+    Type *FloatType = Type::get_float_type(module);
+
+    //main函数
+    auto mainFun = Function::create(FunctionType::get(Int32Type, {}), "main", module);
+    auto bb = BasicBlock::create(module, "entry", mainFun);
+    builder->set_insert_point(bb);
+    auto retAlloca = builder->create_alloca(Int32Type);
+
+    //float b = 1.8;
+    auto bAlloca = builder->create_alloca(FloatType);
+    builder->create_store(CONST_FP(1.8), bAlloca);
+
+    //int a[2] = {2};
+    auto *arrayType_a = ArrayType::get(Int32Type, 2);
+    auto aAlloca = builder->create_alloca(arrayType_a);
+    auto a0Gep = builder->create_gep(aAlloca, {CONST_INT(0), CONST_INT(0)});
+    builder->create_store(CONST_INT(2), a0Gep);
+    auto a1Gep = builder->create_gep(aAlloca, {CONST_INT(0), CONST_INT(1)});
+    builder->create_store(CONST_INT(0), a1Gep);
+
+    //a[1] = a[0] * b;
+    auto a0Load = builder->create_load(a0Gep);
+    auto bLoad = builder->create_load(bAlloca);
+    auto a0Load_float = builder->create_sitofp(a0Load, FloatType);
+    auto mul = builder->create_fmul(a0Load_float, bLoad);
+    auto mul_int = builder->create_fptosi(mul, Int32Type);
+    builder->create_store(mul_int, a1Gep);
+
+    //return a[1];
+    auto a1Load = builder->create_load(a1Gep);
+    builder->create_store(a1Load, retAlloca);
+    auto retLoad = builder->create_load(retAlloca);
+    builder->create_ret(retLoad);
+
+    std::cout << module->print();
+    delete module;
     return 0;
 }
--- Student/task2/cpp/func_gen.cpp
+++ Student/task2/cpp/func_gen.cpp
@@ -8,8 +8,8 @@
 #include <iostream>
 #include <memory>
 
-#ifdef DEBUG                                             // 用于调试信息,大家可以在编译过程中通过" -DDEBUG"来开启这一选项
-#define DEBUG_OUTPUT std::cout << __LINE__ << std::endl; // 输出行号的简单示例
+#ifdef DEBUG
+#define DEBUG_OUTPUT std::cout << __LINE__ << std::endl;
 #else
 #define DEBUG_OUTPUT
 #endif
@@ -18,59 +18,65 @@
     ConstantInt::get(num, module)
 
 #define CONST_FP(num) \
-    ConstantFloat::get(num, module) // 得到常数值的表示,方便后面多次用到
+    ConstantFloat::get(num, module)
 
-int main()
-{
-    auto module = new Module("SysY code"); // module name是什么无关紧要
+int main(){
+    auto module = new Module("SysY code");
     auto builder = new IRStmtBuilder(nullptr, module);
     Type *Int32Type = Type::get_int32_type(module);
+    Type *FloatType = Type::get_float_type(module);
 
+    //add函数
     std::vector<Type *> Ints(2, Int32Type);
     auto addFunTy = FunctionType::get(Int32Type, Ints);
-    auto addFun = Function::create(addFunTy,
-                                   "add", module);
+    auto addFun = Function::create(addFunTy, "add", module);
     auto bb = BasicBlock::create(module, "entry", addFun);
-    builder->set_insert_point(bb); // 一个BB的开始,将当前插入指令点的位置设在bb
-
-    auto retAlloca = builder->create_alloca(Int32Type); // 在内存中分配返回值的位置
-    auto aAlloca = builder->create_alloca(Int32Type);   // 在内存中分配参数n的位置
-    auto bAlloca = builder->create_alloca(Int32Type);   // 在内存中分配参数n的位置
-
-    std::vector<Value *> args; // 获取climbStairs函数的形参,通过Function中的iterator
-    for (auto arg = addFun->arg_begin(); arg != addFun->arg_end(); arg++)
-    {
-        args.push_back(*arg); // * 号运算符是从迭代器中取出迭代器当前指向的元素
+    builder->set_insert_point(bb);
+    auto retAlloca = builder->create_alloca(Int32Type);
+    auto aAlloca = builder->create_alloca(Int32Type);
+    auto bAlloca = builder->create_alloca(Int32Type);
+    std::vector<Value *> args;
+    for (auto arg = addFun->arg_begin(); arg != addFun->arg_end(); arg++) {
+        args.push_back(*arg);
     }
+    builder->create_store(args[0], aAlloca);
+    builder->create_store(args[1], bAlloca);
 
-    builder->create_store(args[0], aAlloca); // store参数a
-    builder->create_store(args[1], bAlloca); // store参数b
+    //return (a+b-1);
     auto aLoad = builder->create_load(aAlloca);
     auto bLoad = builder->create_load(bAlloca);
     auto add = builder->create_iadd(aLoad, bLoad);
-    auto ret = builder->create_isub(add, CONST_INT(1));
-    builder->create_ret(ret);
+    auto sub = builder->create_isub(add, CONST_INT(1));
+    builder->create_store(sub, retAlloca);
+    auto retLoad = builder->create_load(retAlloca);
+    builder->create_ret(retLoad);
 
-    // main 函数
-    auto mainFun = Function::create(FunctionType::get(Int32Type, {}),
-                                    "main", module);
+    //main函数
+    auto mainFun = Function::create(FunctionType::get(Int32Type, {}), "main", module);
     bb = BasicBlock::create(module, "entry", mainFun);
-    // BasicBlock的名字在生成中无所谓,但是可以方便阅读
     builder->set_insert_point(bb);
-
     retAlloca = builder->create_alloca(Int32Type);
+
+    //int a; int b; int c;
     aAlloca = builder->create_alloca(Int32Type);
     bAlloca = builder->create_alloca(Int32Type);
     auto cAlloca = builder->create_alloca(Int32Type);
+
+    //a=3; b=2; c=5;
     builder->create_store(CONST_INT(3), aAlloca);
     builder->create_store(CONST_INT(2), bAlloca);
     builder->create_store(CONST_INT(5), cAlloca);
+
+    //return c + add(a,b);
     aLoad = builder->create_load(aAlloca);
     bLoad = builder->create_load(bAlloca);
-    auto cLoad = builder->create_load(cAlloca);
     auto call = builder->create_call(addFun, {aLoad, bLoad});
-    ret = builder->create_iadd(cLoad, call);
-    builder->create_ret(ret);
+    auto cLoad = builder->create_load(cAlloca);
+    add = builder->create_iadd(cLoad, call);
+    builder->create_store(add, retAlloca);
+    retLoad = builder->create_load(retAlloca);
+    builder->create_ret(retLoad);
+
     std::cout << module->print();
     delete module;
     return 0;
